15723d96c32968a005bf74df98702e0238dc8896
tiemah <doreentiema87@gmail.com>
2023-02-18 10:02:32 -0800

    base 16

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -15,9 +15,13 @@ int main(void)
 
 	for (d = '0'; d <= '9'; d++)
 	  putchar(d);
-	for (low = 'a'; low <= 'f'; low++)
+
+	for (low = 'a'; low <= 'f'; low++){
           putchar(low);
-	  putchar('\n');
+	
+	}
+	putchar('\n');	  
+
 
 	return (0);
 }
