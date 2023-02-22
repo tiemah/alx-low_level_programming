4dfccc742c720f90157948ad843bdd13cc1bbff2
tiemah <doreentiema87@gmail.com>
2023-02-22 09:01:18 -0800

    alphabet letters

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * main - prints Putchar
+ *
+ * Description: prints lowercase alphabets followed by a new line
+ *
+ * Return: 0 (Success)
+ */
+
+void print_alphabet(void)
+{
+        int i;
+	for(i='a'; i>='z'; i++)
+	
+		_putchar(i);
+
+
+	_putchar('\n');
+	
+}
