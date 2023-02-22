d60ce750bd8b52654b01109fe4338bd6b39aeb6c
tiemah <doreentiema87@gmail.com>
2023-02-22 09:09:21 -0800

    printing alphabet 10 times

---
+++ 0x02-functions_nested_loops/2-main.c
@@ -0,0 +1,9 @@
+#include "main.h"
+/**
+ * main.c file
+ */
+int main(void)
+{
+	    print_alphabet_x10();
+	        return (0);
+}
---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * Description: printing the alphabet 10 times"
+ *
+ */
+void print_alphabet_x10(void)
+{
+	char i;
+	int j;
+
+	for (j = 0; j < 10; j++)
+	{
+		for (i = 'a'; i <= 'z'; i++)
+			_putchar(i);
+		_putchar('\n');
+	}
+}
